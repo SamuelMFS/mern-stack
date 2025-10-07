c47dc823951a0257e521571a17053bd7d5b1ebca
SamuelMFS <samuelzin1807@gmail.com>
2025-10-06 21:03:47 -0300

    Video #9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home.js";
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>      
-        < div className="pages">
-
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
         </div>
       </BrowserRouter>
     </div>
